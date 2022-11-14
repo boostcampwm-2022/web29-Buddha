f9ec3368f01c98a240b3e27df4402caf307790be
unho-00 <djsgh94@naver.com>
2022-11-14 16:05:57 +0900

    feat: [FE] 로그인 페이지
    - 로고, 네이버 로그인 이미지 추가
    - 로그인 이미지 클릭 시, OAuth 로그인 요청 추가 필요
    
    #2

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './Layout';
+import Signin from './pages/Signin';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <Layout>
         <BrowserRouter>
           <Routes>
-            <Route path={'/'} element={<></>}></Route> {/* 로그인 */}
+            <Route path={'/'} element={<Signin />}></Route> {/* 로그인 */}
             <Route path={'/signup'} element={<></>}></Route> {/* 회원가입 */}
             <Route path={'/mypage'} element={<></>}></Route> {/* 마이페이지 */}
             <Route path={'/home'} element={<></>}></Route> {/* 고객 메인 */}

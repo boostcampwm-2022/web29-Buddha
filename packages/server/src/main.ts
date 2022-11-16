6b3e6363ee7d46adb9a97e1d422ce512cc5409e6
Gon Kim <geonkim23@gmail.com>
2022-11-16 12:30:32 +0900

    chore, feat: [BE] session-file-store 설치, session 기본 설정
    httponly 활성화
    secure은 추후 https 설정 후 활성화 예정
    자동 저장 x. 수동 저장

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import session from 'express-session';
+import sessionFileStore from 'session-file-store';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    session({
+      secret: 'buddah!@#$',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        secure: false,
+        httpOnly: true,
+      },
+      store: new sessionFileStore(session)(),
+      name: 'sid',
+    })
+  );
   await app.listen(8080);
 }
 bootstrap();

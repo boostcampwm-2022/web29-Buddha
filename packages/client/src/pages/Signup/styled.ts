886b7f60954a184c1a88e1cafa5aa3748de71e22
qmdl980 <qmdl98056@gmail.com>
2022-11-16 11:17:21 +0900

    feat: [FE] Input 스타일링 적용

--- packages/client/src/pages/Signup/styled.ts
+++ packages/client/src/pages/Signup/styled.ts
@@ -6,17 +6,22 @@ export const PageWrapper = styled.div`
   align-items: center;
   gap: 3rem;
   width: 100%;
-  height: 100%;
-  padding-top: 5rem;
+  padding-top: 3rem;
 `;
 
-export const ChangeForm = styled.div``;
+export const ChangeForm = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 50%;
+`;
 
 export const ChangeButton = styled.span`
-  padding: 0.8rem 1.5rem 0.8rem 1.5rem;
+  width: 50%;
+  padding: 0.8rem 0 0.8rem 0;
   font-size: ${(props) => props.theme.font.size.sm};
   font-weight: ${(props) => props.theme.font.weight.bold700};
   border-bottom: ${(props) => `2px solid ${props.theme.colors.grey600}`};
+  text-align: center;
 
   &.selected {
     border-bottom: ${(props) => `4px solid ${props.theme.colors.primary}`};
@@ -31,11 +36,19 @@ export const InputWrapper = styled.div`
 `;
 
 export const InputTitle = styled.p`
-  font-size: ${(props) => `${props.theme.font.size.xs}`};
+  font-size: ${(props) => props.theme.font.size.xs};
 `;
 
 export const Input = styled.input`
   width: 100%;
+  padding: 0 0 0.2rem 0;
+  font-size: ${(props) => props.theme.font.size.sm};
+  border: none;
+  border-bottom: ${(props) => `1px solid ${props.theme.colors.grey400}`};
+
+  &:focus {
+    outline: none;
+  }
 `;
 
 export const CustomButton = styled.button`

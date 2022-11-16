460e3cf241b4824116fd4b8b4f0f14e9d222c071
qmdl980 <qmdl98056@gmail.com>
2022-11-16 10:54:13 +0900

    style: [FE] CSS 우선순위 및 코드 스타일 변경

--- packages/client/src/pages/Signup/styled.ts
+++ packages/client/src/pages/Signup/styled.ts
@@ -4,22 +4,19 @@ export const PageWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  gap: 3rem;
   width: 100%;
   height: 100%;
   padding-top: 5rem;
-  gap: 3rem;
 `;
 
 export const ChangeForm = styled.div``;
 
 export const ChangeButton = styled.span`
   padding: 0.8rem 1.5rem 0.8rem 1.5rem;
-
-  ${(props) => ({
-    borderBottom: `2px solid ${props.theme.colors.grey600}`,
-    fontSize: props.theme.font.size.sm,
-    fontWeight: props.theme.font.weight.bold700,
-  })}
+  font-size: ${(props) => props.theme.font.size.sm};
+  font-weight: ${(props) => props.theme.font.weight.bold700};
+  border-bottom: ${(props) => `2px solid ${props.theme.colors.grey600}`};
 
   &.selected {
     border-bottom: ${(props) => `4px solid ${props.theme.colors.primary}`};
@@ -44,13 +41,10 @@ export const Input = styled.input`
 export const CustomButton = styled.button`
   width: 80%;
   padding: 0.3rem 0 0.3rem 0;
+  background-color: ${(props) => props.theme.colors.primary};
   color: white;
+  font-size: ${(props) => props.theme.font.size.xs};
+  font-weight: ${(props) => props.theme.font.weight.bold700};
   border: none;
   border-radius: 50px;
-
-  ${(props) => ({
-    backgroundColor: props.theme.colors.primary,
-    fontSize: props.theme.font.size.xs,
-    fontWeight: props.theme.font.weight.bold700,
-  })}
 `;

ad000b02cd411d7f051bb77ffcf7dad0740a060d
Gon Kim <geonkim23@gmail.com>
2022-11-16 14:34:02 +0900

    refactor: [BE] user/auth 모듈 의존성 변경
    auth 모듈에는 인증, 권한 확인, oauth 로직을 두고, user에서 이를 가져와 사용하는 방식으로 변경

--- packages/server/src/user/user.module.ts
+++ packages/server/src/user/user.module.ts
@@ -3,11 +3,12 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), AuthModule],
   controllers: [UserController],
   providers: [UserService],
-  exports: [UserService], // User repository 만든걸 다른 곳에서도 사용하고 싶은 경우
+  // exports: [UserService], // User repository 만든걸 다른 곳에서도 사용하고 싶은 경우
 })
 export class UserModule {}

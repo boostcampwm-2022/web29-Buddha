c3da7d38672325b62c1ce45097dc0405e5d9ebb2
qmdl980 <qmdl98056@gmail.com>
2022-11-22 14:45:41 +0900

    refactor: [FE] 메뉴 리스트 페이지 테스트 코드 리팩토링
    - mock 서버 핸들러 코드 분리

--- packages/client/src/pages/MenuList/index.spec.tsx
+++ packages/client/src/pages/MenuList/index.spec.tsx
@@ -1,44 +1,9 @@
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import { rest } from 'msw';
-import { setupServer } from 'msw/node';
+import { server } from '@/mocks/server';
 import { MemoryRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import Router from '@/Router';
 import Layout from '@/Layout';
-
-const api = process.env.REACT_APP_API_SERVER_BASE_URL;
-
-const server = setupServer(
-  rest.get(`${api}/cafe/1/menus`, (req, res, next) => {
-    return res(
-      next.json({
-        id: 1,
-        cafe_name: 'Cafe name 1',
-        menus: [
-          {
-            id: 1,
-            name: '카페 아메리카노',
-            thumbnail:
-              'https://www.istarbucks.co.kr/upload/store/skuimg/2022/10/[9200000004312]_20221005145029134.jpg',
-            price: 1000,
-          },
-          {
-            id: 2,
-            name: 'Menu name 2',
-            thumbnail: 'Menu Thumbnail',
-            price: 1000,
-          },
-          {
-            id: 3,
-            name: 'Menu name 3',
-            thumbnail: 'Menu Thumbnail',
-            price: 1000,
-          },
-        ],
-      })
-    );
-  })
-);
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());

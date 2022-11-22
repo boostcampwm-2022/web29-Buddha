3f943d124b00e72c83515edf8def46c26dd4b470
qmdl980 <qmdl98056@gmail.com>
2022-11-22 23:21:20 +0900

    feat: [FE] 장바구니 페이지 기본 레이아웃 구성
    #72

---
+++ packages/client/src/pages/customer/Cart/index.tsx
@@ -0,0 +1,16 @@
+function Cart() {
+  return (
+    <div>
+      <div>
+        <p>장바구니</p>
+        <p>주문할 매장을 선택해주세요</p>
+      </div>
+      <p>담은 상품 0개</p>
+      <div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Cart;

5275d51de5be4ab5d1ab97dd182059db279c7889
qmdl980 <qmdl98056@gmail.com>
2022-11-22 23:58:28 +0900

    feat: [FE] 장바구니 페이지 기본 구성
    - 디자인 초안 작성
    - 내부 콘텐츠 컴포넌트 필요
    
    #72

--- packages/client/src/pages/customer/Cart/index.tsx
+++ packages/client/src/pages/customer/Cart/index.tsx
@@ -1,16 +1,19 @@
 import CartFooter from '@/components/CartFooter';
+import { CartPageWrapper, CartHeader, CartContentWrapper } from './styled';
 
 function Cart() {
   return (
-    <div>
-      <div>
-        <p>장바구니</p>
-        <p>주문할 매장을 선택해주세요</p>
-      </div>
-      <p>담은 상품 0개</p>
-      <div></div>
+    <CartPageWrapper>
+      <CartHeader>
+        <p className={'title'}>장바구니</p>
+        <p className={'input-cafe'}>주문할 매장을 선택해주세요</p>
+      </CartHeader>
+      <CartContentWrapper>
+        <p className={'title'}>담은 상품 0개</p>
+        <div></div>
+      </CartContentWrapper>
       <CartFooter />
-    </div>
+    </CartPageWrapper>
   );
 }
 
--- packages/client/src/pages/customer/Cart/styled.ts
+++ packages/client/src/pages/customer/Cart/styled.ts
@@ -1,1 +1,33 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
+
+export const CartPageWrapper = styled.div`
+  width: 100%;
+`;
+
+export const CartHeader = styled.div`
+  width: 100%;
+  padding: 2rem 1.5rem 0.6rem 1.5rem;
+  background-color: ${(props) => props.theme.colors.secondary};
+  color: white;
+
+  .title {
+    margin-bottom: 0.6rem;
+    font-size: ${(props) => props.theme.font.size.lg};
+  }
+
+  .input-cafe {
+    padding-bottom: 2px;
+    font-size: ${(props) => props.theme.font.size.xs};
+    border-bottom: 1px solid white;
+  }
+`;
+
+export const CartContentWrapper = styled.div`
+  width: 100%;
+  padding: 1rem 1.5rem 5rem 1.5rem;
+
+  .title {
+    font-size: ${(props) => props.theme.font.size.sm};
+    font-weight: ${(props) => props.theme.font.weight.bold700};
+  }
+`;

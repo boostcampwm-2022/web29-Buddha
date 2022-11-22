8cedbc267c15812137efe292b40060bd20057093
qmdl980 <qmdl98056@gmail.com>
2022-11-22 16:25:29 +0900

    refactor: [FE] Mock 서버 응답 데이터 분리

--- packages/client/src/mocks/handlers/menu.ts
+++ packages/client/src/mocks/handlers/menu.ts
@@ -1,87 +1,21 @@
 import { rest } from 'msw';
+import { menuDetailData, menuListData } from '@/mocks/data/menu';
 
 const api = process.env.REACT_APP_API_SERVER_BASE_URL;
 
-const jsonMenuDetail = {
-  id: 1,
-  name: '자몽 허니 블랙 티',
-  description:
-    '새콤한 자몽과 달콤한 꿀이 깊고 그윽한 풍미의 스타벅스 티바나 블랙 티의 조합',
-  price: 5700,
-  thumbnail:
-    'https://www.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000000187]_20210419131229539.jpg',
-  options: [
-    {
-      id: 1,
-      name: '1',
-      price: 500,
-      category: '에스프레소 샷',
-    },
-    {
-      id: 2,
-      name: '2',
-      price: 1000,
-      category: '에스프레소 샷',
-    },
-    {
-      id: 3,
-      name: '3',
-      price: 1500,
-      category: '에스프레소 샷',
-    },
-    {
-      id: 4,
-      name: '1',
-      price: 500,
-      category: '클래식 시럽',
-    },
-    {
-      id: 5,
-      name: '2',
-      price: 1000,
-      category: '클래식 시럽',
-    },
-  ],
-};
-
 export const menuHandlers = [
   rest.get(`${api}/cafe/1/menus`, (req, res, next) => {
-    return res(
-      next.json({
-        id: 1,
-        cafe_name: 'Cafe name 1',
-        menus: [
-          {
-            id: 1,
-            name: '카페 아메리카노',
-            thumbnail:
-              'https://www.istarbucks.co.kr/upload/store/skuimg/2022/10/[9200000004312]_20221005145029134.jpg',
-            price: 1000,
-          },
-          {
-            id: 2,
-            name: 'Menu name 2',
-            thumbnail: 'Menu Thumbnail',
-            price: 1000,
-          },
-          {
-            id: 3,
-            name: 'Menu name 3',
-            thumbnail: 'Menu Thumbnail',
-            price: 1000,
-          },
-        ],
-      })
-    );
+    return res(next.json(menuListData));
   }),
   // 메뉴 상세 정보 조회
-  rest.get(`${api}/cafe/menu/:menuId`, (req, res, ctx) => {
+  rest.get(`${api}/cafe/menu/:menuId`, (req, res, next) => {
     const { menuId } = req.params;
 
     switch (menuId) {
       case '1':
-        return res(ctx.json(jsonMenuDetail));
+        return res(next.json(menuDetailData));
       default:
-        return res(ctx.status(400));
+        return res(next.status(400));
     }
+  }),
 ];

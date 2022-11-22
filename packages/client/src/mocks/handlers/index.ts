63e9ecabf03eff0cc9608c94daaa4c2415eee0b6
unho-00 <djsgh94@naver.com>
2022-11-22 14:14:38 +0900

    test: [FE] 옵션 선택 컴포넌트 테스트 작성

--- packages/client/src/mocks/handlers/index.ts
+++ packages/client/src/mocks/handlers/index.ts
@@ -1,3 +1,4 @@
 import { authHandlers } from './auth';
+import { menuHandlers } from './menu';
 
-export const handlers = [...authHandlers];
+export const handlers = [...authHandlers, ...menuHandlers];
---
+++ packages/client/src/mocks/handlers/menu.ts
@@ -0,0 +1,51 @@
+import { rest } from 'msw';
+
+const api = process.env.REACT_APP_API_SERVER_BASE_URL;
+
+const jsonMenuDetail = {
+  id: 1,
+  name: '자몽 허니 블랙 티',
+  description:
+    '새콤한 자몽과 달콤한 꿀이 깊고 그윽한 풍미의 스타벅스 티바나 블랙 티의 조합',
+  price: 5700,
+  thumbnail:
+    'https://www.istarbucks.co.kr/upload/store/skuimg/2021/04/[9200000000187]_20210419131229539.jpg',
+  options: [
+    {
+      id: 1,
+      name: '1',
+      price: 500,
+      category: '에스프레소 샷',
+    },
+    {
+      id: 2,
+      name: '2',
+      price: 1000,
+      category: '에스프레소 샷',
+    },
+    {
+      id: 3,
+      name: '3',
+      price: 1500,
+      category: '에스프레소 샷',
+    },
+    {
+      id: 4,
+      name: '1',
+      price: 500,
+      category: '클래식 시럽',
+    },
+    {
+      id: 5,
+      name: '2',
+      price: 1000,
+      category: '클래식 시럽',
+    },
+  ],
+};
+export const menuHandlers = [
+  // 메뉴 상세 정보 조회
+  rest.get(`${api}/cafe/menu/:menuId`, (req, res, ctx) => {
+    return res(ctx.json(jsonMenuDetail));
+  }),
+];

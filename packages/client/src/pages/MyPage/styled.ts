a3abfec5637f17813d1ae11d4b4c981f3da35ad2
오혁진 <qmdl98056@gmail.com>
2022-11-24 14:46:06 +0900

    feat: [FE] 마이 페이지 기본 레이아웃 작업

---
+++ packages/client/src/pages/MyPage/styled.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const MyPageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 0.3rem;
+`

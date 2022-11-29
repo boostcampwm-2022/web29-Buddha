7e0fe7454426f05ab08303c0977c1aae08326407
Gon Kim <geonkim23@gmail.com>
2022-11-29 20:30:06 +0900

    feat: [BE] be dev cd 추가

---
+++ packages/server/dev.deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# 환경변수 밖으로 빼주기
+mv .dev.env ..
+
+# 이미지 pull
+docker pull $1/$2:latest
+
+# 컨테이너 내리고, 띄우기
+if [[ $(docker ps -a --filter="name=node-nest" --filter "status=running" | grep -w node-nest) ]]; then
+    docker stop node-test
+fi
+if [[ $(docker ps -a --filter="name=node-test" --filter "status=created" | grep -w node-test) ]]; then
+    docker rm node-test
+fi
+if [[ $(docker ps -a --filter="name=node-test" --filter "status=exited" | grep -w node-test) ]]; then
+    docker rm node-test
+fi
+
+docker run -p 8080:8080 -v ~/server:/app/dist -v -d --name node-test $1/$2:latest
+
+exit 0

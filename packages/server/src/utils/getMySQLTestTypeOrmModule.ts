e3ca4e090fab7a713cd7867f080fc59baf0dc099
jinoo7099 <vxc7099@gmail.com>
2022-12-01 12:32:25 +0900

    feat: [BE] test database 접속용 typeORM Module util 추가

---
+++ packages/server/src/utils/getMySQLTestTypeOrmModule.ts
@@ -0,0 +1,20 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as path from 'path';
+
+// test db 접속용
+export function getMySQLTestTypeOrmModule() {
+  const entityPath = path.join(process.env.PWD, 'src/**/*.entity{.ts,.js}');
+  return TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: process.env.MYSQL_HOST,
+    port: parseInt(process.env.MYSQL_PORT),
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    entities: [entityPath],
+    synchronize: true,
+    logging: true,
+    dropSchema: true,
+  });
+}

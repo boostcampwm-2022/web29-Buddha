53833a7503e60704f8880048a30e557a1f62fc81
jinoo7099 <vxc7099@gmail.com>
2022-12-01 12:41:39 +0900

    feat: [BE] 테스트용 환경변수 업로드 파일 이름 수정

--- packages/server/src/setEnvVar.ts
+++ packages/server/src/setEnvVar.ts
@@ -1,17 +1,17 @@
 import { existsSync, readFileSync } from 'fs';
 import path from 'path';
 
-const envPath = path.join(process.env.PWD, '.dev.env');
+const TEST_ENV_NAME = '.test.env';
+const envPath = path.join(process.env.PWD, TEST_ENV_NAME);
 if (!existsSync(envPath)) {
   throw new Error('환경변수 로드가 안됐습니다.');
 }
-const env = readFileSync(path.join(process.env.PWD, '.dev.env')).toString();
+const env = readFileSync(path.join(process.env.PWD, TEST_ENV_NAME)).toString();
 
 const lines = env.split('\n').filter((el) => el !== '');
 
 lines.forEach((el) => {
   const [key, value] = el.split('=');
-  console.log(key, value);
 
   process.env[key] = value;
 });

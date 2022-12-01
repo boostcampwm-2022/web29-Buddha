c69ec4ffadb3260cc0d53847a735cdc389e448e0
jinoo7099 <vxc7099@gmail.com>
2022-12-01 12:33:06 +0900

    test: [BE] Cafe Service - findAllMenuById 통합테스트 작성

---
+++ packages/server/src/auth/test/integration/cafe/CafeService.int.spec.ts
@@ -0,0 +1,66 @@
+import { CafeService } from '../../../../cafe/cafe.service';
+import { CafeModule } from 'src/cafe/cafe.module';
+import { Cafe } from 'src/cafe/entities/cafe.entity';
+import { Repository } from 'typeorm';
+import { Test, TestingModule } from '@nestjs/testing';
+import { getMySQLTestTypeOrmModule } from 'src/utils/getMySQLTestTypeOrmModule';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Menu } from 'src/cafe/entities/menu.entity';
+
+describe('Cafe Service', () => {
+  let sut: CafeService;
+  let cafeRepository: Repository<Cafe>;
+  let menuRepository: Repository<Menu>;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [CafeModule, getMySQLTestTypeOrmModule()],
+      providers: [
+        CafeService,
+        // mock Repository가 아니라 진짜 typeORM이 만들어준 Repository 사용
+        {
+          provide: getRepositoryToken(Cafe),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(Menu),
+          useClass: Repository,
+        },
+      ],
+    }).compile();
+
+    sut = module.get<CafeService>(CafeService);
+    cafeRepository = module.get<Repository<Cafe>>(getRepositoryToken(Cafe));
+    menuRepository = module.get<Repository<Menu>>(getRepositoryToken(Menu));
+  });
+
+  beforeEach(async () => {
+    await cafeRepository.query('set foreign_key_checks = 0');
+    await cafeRepository.clear();
+    await cafeRepository.query('set foreign_key_checks = 1');
+
+    await menuRepository.query('set foreign_key_checks = 0');
+    await menuRepository.clear();
+    await menuRepository.query('set foreign_key_checks = 1');
+  });
+
+  it('findAllMenuById', async () => {
+    //given
+    const cafe = Cafe.of({
+      id: 1,
+      name: '스타벅스',
+      description: '대야동점',
+      latitude: 36,
+      longitude: 40,
+      address: '경기도 시흥시 은행로',
+    });
+    await cafeRepository.save(cafe);
+
+    // when
+    const users = await sut.findAllMenuById(cafe.id);
+
+    // then
+    expect(users.id).toBe(cafe.id);
+    expect(users.name).toBe(cafe.name);
+  });
+});

cd65caeb54b909b015d0e6cd8421c42b0b25e4ef
jinoo7099 <vxc7099@gmail.com>
2022-12-02 11:08:52 +0900

    feat: [BE] HTTP 요청 로그 출력하는 middleware 추가

---
+++ packages/server/src/middleware/logger.http.ts
@@ -0,0 +1,20 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { Logger } from '@nestjs/common';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP');
+
+  use(req: Request, res: Response, next: NextFunction) {
+    const { ip, method, originalUrl } = req;
+    const userAgent = req.get('user-agent') || '';
+    res.on('finish', () => {
+      const { statusCode } = res;
+      this.logger.log(
+        `${method} ${statusCode} - ${originalUrl} - ${ip} - ${userAgent}`
+      );
+    });
+    next();
+  }
+}

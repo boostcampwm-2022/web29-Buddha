3fc9e6e96840bee692073b775f75b5d9cbd0a03b
jinoo7099 <vxc7099@gmail.com>
2022-12-03 22:13:45 +0900

    refactor: [BE] 글로벌 미들웨어, 필터 등을 등록해주는 함수 분리

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,3 +1,4 @@
+import { setNestApp } from 'src/setNestApp';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import session from 'express-session';
@@ -14,25 +15,7 @@ declare module 'express-session' {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalFilters(new HttpExceptionFilter());
-  app.enableCors({
-    origin: process.env.CLIENT_URI,
-    credentials: true,
-  });
-  app.use(cookieParser());
-  app.use(
-    session({
-      secret: 'buddah!@#$',
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        maxAge: 1000 * 60 * 3,
-        secure: false,
-        httpOnly: true,
-      },
-      name: 'sid',
-    })
-  );
+  setNestApp(app);
   await app.listen(8080);
 }
 bootstrap();

1d6b222ec7b232cd2dfe1708af9aa57bdccf032a
jinoo7099 <vxc7099@gmail.com>
2022-12-07 15:44:00 +0900

    feat: [BE] redis config 설정
    redis module 생성
    redis store  연결

--- packages/server/src/order/order.module.ts
+++ packages/server/src/order/order.module.ts
@@ -5,9 +5,13 @@ import { OrderController } from './order.controller';
 import { Order } from './entities/order.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MenuOption } from 'src/cafe/entities/menuOption.entity';
+import { RedisCacheModule } from 'src/redisCache/redisCache.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, MenuOption, OrderMenu])],
+  imports: [
+    TypeOrmModule.forFeature([Order, MenuOption, OrderMenu]),
+    RedisCacheModule,
+  ],
   controllers: [OrderController],
   providers: [OrderService],
 })

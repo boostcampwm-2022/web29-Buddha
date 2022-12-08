7ccd6daf1752cb75835dcb8ea77b2df1367b1a8a
unho-00 <djsgh94@naver.com>
2022-12-08 23:21:50 +0900

    fix: [FE] Query Key 값 수정
    - 하이픈 추가

--- packages/client/src/constants.ts
+++ packages/client/src/constants.ts
@@ -30,11 +30,11 @@ export const PROGRESS_IMAGE = {
 };
 
 export const QUERY_KEYS = {
-  MENU_LIST_DATA: 'menu list',
-  ORDER_STATUS: 'order status',
-  USER_ROLE: 'user role',
-  ORDER_LIST: 'order list',
-  ACCEPTED_LIST: 'accepted_list',
+  MENU_LIST_DATA: 'menu-list',
+  ORDER_STATUS: 'order-status',
+  USER_ROLE: 'user-role',
+  ORDER_LIST: 'order-list',
+  ACCEPTED_LIST: 'accepted-list',
 };
 
 export const USER_ROLE = {

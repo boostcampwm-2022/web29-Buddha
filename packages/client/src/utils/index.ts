0eb8b69a8a28cd80051d0cd418ebb46b07ca64d6
unho-00 <djsgh94@naver.com>
2022-12-12 22:20:54 +0900

    fix: [FE] 주문 내역 정렬 및 필터링 수정
    - 주문 내역 내림차순 정렬
    - 고객) 하단 목록 -> 주문 완료 목록만 필터링

--- packages/client/src/utils/index.ts
+++ packages/client/src/utils/index.ts
@@ -11,3 +11,20 @@ export const getPriceComma = (price: number | string) => {
 export const getFirstUpper = (text: string) => {
   return `${text.slice(0, 1).toUpperCase()}${text.slice(1)}`;
 };
+
+/**
+ * 문자열 날짜를 숫자로 변환하여 반환
+ *
+ * @params (string) YYYY-MM-DD
+ * @returns (number) YYYYMMDD
+ */
+export const getDateNumber = (date: string) =>
+  Number(date.slice(0, 10).replace(/-/g, ''));
+
+/**
+ * 주문 내역 날짜를 내림차순으로 정렬
+ */
+export const sortDateDesc = (prev: string, curr: string) => {
+  if (prev === '오늘') return -1;
+  return getDateNumber(curr) - getDateNumber(prev);
+};
